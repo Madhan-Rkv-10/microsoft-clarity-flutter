9bf4ad57544fda1b4f64c585a91dab318f51a4a2
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-07-16 21:32:26 +0530

    chore: update for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'dart:async';
+// import 'dart:async';
+
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:clarity_flutter/clarity_flutter.dart';
@@ -21,16 +23,90 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigatorKey,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Clarity Flutter SDK Example')),
-        body: ClarityMask(
-          child: Column(children: [Center(child: Text('Hello, Clarity!'))]),
+      checkerboardOffscreenLayers: true,
+      onNavigationNotification: (notification) {
+        log("Notification$notification");
+        return true;
+      },
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Clarity Flutter SDK Example')),
+      body: ClarityMask(
+        child: Column(
+          children: [
+            Center(child: Text('Hello, Clarity!')),
+            TextButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => A()));
+              },
+              child: Text("A to Navigate"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => A()));
+              },
+              child: Text("A to Navigate"),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class A extends StatefulWidget {
+  const A({super.key});
+
+  @override
+  State<A> createState() => _AState();
+}
+
+class _AState extends State<A> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: TextButton(
+        onPressed: () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => B()));
+        },
+        child: Text("A to Navigate"),
+      ),
+    );
+  }
+}
+
+class B extends StatefulWidget {
+  const B({super.key});
+
+  @override
+  State<B> createState() => _BState();
+}
+
+class _BState extends State<B> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: Text("data"));
   }
 }
